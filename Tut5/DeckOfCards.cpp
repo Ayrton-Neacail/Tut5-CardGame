7239ea9bfbdc2ff3bfc975decab114f5d6f24226
Ayrton-Neacail <ayrtona90@gmail.com>
2016-03-23 12:53:55 +0200

    added the member functions

--- Tut5/DeckOfCards.cpp
+++ Tut5/DeckOfCards.cpp
@@ -2,14 +2,74 @@
 #include <string>
 #include "DeckOfCards.h"
 #include <cstdlib>
-#include <time.h>
+
+
+#include <ctime>
 
 using namespace std;
 
-DeckOfCards::DeckOfCards(){
 
-
+DeckOfCards::DeckOfCards()
+{
 	Total = 20;
-	PtrCard = new Card[Total]
+	Ptr = new Card[Total];
+
+	for (int i = 0; i < 10; i++)
+	{
+		Ptr[i] = Card(i + 1, "Red");
+
+		Ptr[10 + i] = Card(i + 1, "Black");
+	}
+}
+
+
+DeckOfCards::~DeckOfCards()
+{
+	delete[] Ptr;
+}
+
+DeckOfCards DeckOfCards::reset()
+{
+	return DeckOfCards();
+}
+
+void DeckOfCards::shuffle()
+{
+	int rndm1;
+	int rndm2;
+	Card temp;
+	srand(time(0));
+	if (Total >= 2)
+	{
+		for (int i = 0; i < 50; i++)
+		{
+			rndm1 = rand() % Total;
+			rndm2 = rand() % Total;
+
+			temp = Ptr[rndm1];
+			Ptr[rndm1] = Ptr[rndm2];
+			Ptr[rndm2] = temp;
+		}
+	}
+}
+
+Card DeckOfCards::draw()
+{
+	Card  temp;
+	temp = Ptr[Total - 1];
+	Total--;
+	return temp;
+	delete &temp;
 
 }
+
+void DeckOfCards::peek()
+{
+
+}
+
+void DeckOfCards::numberOfCards()
+{
+
+}
+
