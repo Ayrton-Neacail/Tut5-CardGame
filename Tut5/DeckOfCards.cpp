bb78a7135d0848d93db5ff1940522032d6e33df3
Ayrton-Neacail <ayrtona90@gmail.com>
2016-03-23 12:46:21 +0200

    coded classes for deck  of cards

---
+++ Tut5/DeckOfCards.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "DeckOfCards.h"
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+DeckOfCards::DeckOfCards(){
+
+
+	Total = 20;
+	PtrCard = new Card[Total]
+
+}
